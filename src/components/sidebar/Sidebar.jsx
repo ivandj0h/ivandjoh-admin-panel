99521341b27ac25b63d10f59c7eed420e5f8cd24
ivandjoh <ivandi.djoh@gmail.com>
2022-05-08 02:43:36 +0800

    modified sidebar menus by updated some scss

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,16 @@
 import './sidebar.scss'
 import DashboardIcon from '@mui/icons-material/Dashboard';
-
+import GroupIcon from '@mui/icons-material/Group';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
+import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import SettingsIcon from '@mui/icons-material/Settings';
+import PersonIcon from '@mui/icons-material/Person';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 const Sidebar = () => {
   return (
@@ -11,43 +21,65 @@ const Sidebar = () => {
         <hr />
         <div className='center'>
           <ul>
+            <p className="title">MAIN MENU</p>
             <li>
-              <DashboardIcon />
+              <DashboardIcon className='icon' />
               <span>Dashboard</span>
             </li>
+            <p className="title">LISTS MENU</p>
             <li>
+              <GroupIcon className='icon' />
               <span>Users</span>
             </li>
             <li>
+              <Inventory2Icon className='icon' />
               <span>Products</span>
             </li>
             <li>
+              <ProductionQuantityLimitsIcon className='icon' />
               <span>Orders</span>
             </li>
             <li>
+              <LocalShippingIcon className='icon' />
+              <span>Delivery</span>
+            </li>
+            <p className="title">OTHER MENU</p>
+            <li>
+              <QueryStatsIcon className='icon' />
               <span>Status</span>
             </li>
             <li>
+              <NotificationsIcon className='icon' />
               <span>Notifications</span>
             </li>
+            <p className="title">SERVICES</p>
             <li>
+              <HealthAndSafetyIcon className='icon' />
               <span>System Health</span>
             </li>
             <li>
+              <PsychologyIcon className='icon' />
               <span>Logs</span>
             </li>
             <li>
+              <SettingsIcon className='icon' />
               <span>Settings</span>
             </li>
+            <p className="title">ACCOUNT</p>
             <li>
+              <PersonIcon className='icon' />
               <span>Profile</span>
             </li>
             <li>
+              <ExitToAppIcon className='icon' />
               <span>Logout</span>
             </li>
           </ul>
         </div>
-        <div className='bottom'>Color Options</div>
+        <div className='bottom'>
+          <div className='colorOptions'></div>
+          <div className='colorOptions'></div>
+        </div>
     </div>
   )
 }
